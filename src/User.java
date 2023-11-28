9762b1641f1822545966de53bead3a80d6895040
fiepoul <fipo0001@stud.kea.dk>
2023-11-28 13:07:03 +0100

    User class og role enum oprettet

---
+++ src/User.java
@@ -0,0 +1,41 @@
+public class User {
+    private String username;
+    private String password;
+    private Role role;
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String password) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String toString() {
+        return "Bruger{" + "brugernavn: '" + getUsername() + '\'' +
+                "kodeord: " + getPassword() +
+                "rolle: " + getRole() + "}";
+    }
+}
