d2a911e0f38262e55bc06ce4124c53fa2d83309f
fiepoul <fipo0001@stud.kea.dk>
2023-12-09 13:29:56 +0100

    test on addtraining

--- test/java/CompetitiveSwimmerTest.java
+++ test/java/CompetitiveSwimmerTest.java
@@ -11,12 +11,12 @@ class CompetitiveSwimmerTest {
         Address address = new Address("vejvej", "4", "2100", "byby");
         CompetitiveSwimmer competitiveSwimmer = new CompetitiveSwimmer("camilla", LocalDate.of(1995, 2, 9), address, "40404040", "fi@fi.dk", true);
 
-        competitiveSwimmer.addTrainingResult(new Result(LocalDate.of(2021, 1, 1), "00:55.00", Discipline.BUTTERFLY));
-        competitiveSwimmer.addTrainingResult(new Result(LocalDate.of(2021, 6, 1), "00:54.00", Discipline.BUTTERFLY));
-        competitiveSwimmer.addTrainingResult(new Result(LocalDate.of(2020, 8, 2), "00:53.00", Discipline.BUTTERFLY)); // Samme tid som ved stævne 2, ældre dato
+        competitiveSwimmer.addTrainingResult(new Result(Discipline.BUTTERFLY, LocalDate.of(2021, 1, 1), "00:55.00"));
+        competitiveSwimmer.addTrainingResult(new Result(Discipline.BUTTERFLY, LocalDate.of(2021, 6, 1), "00:54.00"));
+        competitiveSwimmer.addTrainingResult(new Result(Discipline.BUTTERFLY, LocalDate.of(2020, 8, 2), "00:53.00")); // Samme tid som ved stævne 2, ældre dato
 
-        competitiveSwimmer.addCompetitionResult(new ResultCompetition(LocalDate.of(2021, 5, 1), "00:55.00", Discipline.BUTTERFLY, "Stævne 1", 1));
-        competitiveSwimmer.addCompetitionResult(new ResultCompetition(LocalDate.of(2021, 6, 1), "00:53.00", Discipline.BUTTERFLY, "Stævne 2", 2));
+        competitiveSwimmer.addCompetitionResult(new ResultCompetition(Discipline.BUTTERFLY, LocalDate.of(2021, 5, 1), "00:55.00", "Stævne 1", 1));
+        competitiveSwimmer.addCompetitionResult(new ResultCompetition(Discipline.BUTTERFLY, LocalDate.of(2021, 6, 1), "00:53.00", "Stævne 2", 2));
 
         Result bestResult = competitiveSwimmer.getBestResult(Discipline.BUTTERFLY);
 
@@ -28,4 +28,18 @@ class CompetitiveSwimmerTest {
        System.out.println(bestCompetitionResult);
        System.out.println(bestResult);
     }
+
+    @Test
+    void addTrainingResultReturnsNewestResultWhenIdenticalTime() {
+        Address address = new Address("vejvej", "4", "2100", "byby");
+        CompetitiveSwimmer competitiveSwimmer = new CompetitiveSwimmer("camilla", LocalDate.of(1995, 2, 9), address, "40404040", "fi@fi.dk", true);
+
+        competitiveSwimmer.addTrainingResult(new Result(Discipline.BUTTERFLY, LocalDate.of(2021, 1, 1), "00:55.00"));
+        competitiveSwimmer.addTrainingResult(new Result(Discipline.BUTTERFLY, LocalDate.of(2020, 8, 2), "00:55.00"));
+
+        assertEquals(LocalDate.of(2021, 1, 1), competitiveSwimmer.getTrainingResults().get(Discipline.BUTTERFLY).getDate());
+
+    }
+
+
 }
