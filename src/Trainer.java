3b29d0119f7b5c3f67bc7d0d63e6a2a37779664a
fiepoul <fipo0001@stud.kea.dk>
2023-12-10 08:53:13 +0100

    Trainer attributter added

--- src/Trainer.java
+++ src/Trainer.java
@@ -1,3 +1,12 @@
 public class Trainer {
+    private String name;
+    private String team; //junior eller senior
+    private String pay; //måske ingen betaling
+
+    public Trainer(String name, String team, String pay) {
+        this.name = name;
+        this.team = team;
+        this.pay = pay;
+    }
 
 }
